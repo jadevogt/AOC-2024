31fead69d3b6e3554f3303eb0aba051041ad1dca
Jade Vogt <me@jadev.dev>
2024-12-02 23:44:22 -0500

    Add solution for Day 2

--- src/main/java/com/expedient/adventofcodejade/common/PuzzleInput.java
+++ src/main/java/com/expedient/adventofcodejade/common/PuzzleInput.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Handles both sample and actual input files
@@ -21,6 +22,7 @@ public class PuzzleInput {
 
     /**
      * Constructs a PuzzleInput from a given filesystem path
+     *
      * @param inputPath path where the input is located
      * @return PuzzleInput object that contains the file's contents
      * @throws IOException when the file is not found
@@ -32,6 +34,7 @@ public class PuzzleInput {
 
     /**
      * Constructs a PuzzleInput from the given resource path
+     *
      * @param resourcePath path of resource (contained in /src/main/resources)
      * @return PuzzleInput object that contains the resource's contents
      * @throws IOException when the given resource can't be loaded
@@ -47,6 +50,7 @@ public class PuzzleInput {
 
     /**
      * Gets a PuzzleInput using the resource for a specific day (located in /src/main/resources)
+     *
      * @param day day for which the input is loaded
      * @return PuzzleInput object containing the given day's sample input
      * @throws IOException when the given day's input cannot be found in resources
@@ -58,6 +62,7 @@ public class PuzzleInput {
 
     /**
      * Get the lines from the file
+     *
      * @return a list of strings containing the input as-is
      */
     public List<String> getFileLines() {
@@ -67,12 +72,13 @@ public class PuzzleInput {
 
     /**
      * Return two lists based on split lines from the input
-     * @param conversion function used to convert the string into a type of your choice
+     *
+     * @param conversion     function used to convert the string into a type of your choice
      * @param delimiterRegex delimiter used to split the lines
+     * @param <T>            the type contained within the returned lists
      * @return two lists, composed of the elements that were split from each input
-     * @param <T> the type contained within the returned lists
      */
-    public <T> List<List<T>> getTwoLists(Function <String, T> conversion, String delimiterRegex) {
+    public <T> List<List<T>> getTwoLists(Function<String, T> conversion, String delimiterRegex) {
         var listOne = new ArrayList<T>();
         var listTwo = new ArrayList<T>();
         for (var line : this.fileLines) {
@@ -80,7 +86,35 @@ public class PuzzleInput {
             listOne.add(conversion.apply(split[0]));
             listTwo.add(conversion.apply(split[1]));
         }
+        // This has to be a list, can't create an array of a Generic type
         return Arrays.asList(listOne, listTwo);
+    }
+
+    /**
+     * Converts each line to a list of type T values, after iterating over each Character in the line and applying a
+     * provided filter function. Optionally can apply a transformation to the line string before running the filter.
+     *
+     * @param test       Test to determine whether a given value will be included in the list for each line
+     * @param conversion Function to convert the Character value to type T
+     * @param transform  Function to apply to each line before processing it. If this is null, the transformation step is skipped
+     * @param <T>        The desired output type for the operation
+     * @return List of lists composed of values from each line matching the test function and converted to type T
+     */
+    public <T> List<List<T>> allMatchesPerLine(Predicate<Character> test, Function<Character, T> conversion, Function<String, String> transform) {
+        var parsed = new ArrayList<List<T>>();
+        for (var line : this.fileLines) {
+            var l = line;
+            if (transform != null) {
+                l = transform.apply(l);
+            }
+            var primitiveCharArray = l.toCharArray();
+            Character[] chars = new Character[primitiveCharArray.length];
+            for (int i = 0; i < primitiveCharArray.length; i++) {
+                chars[i] = primitiveCharArray[i];
+            }
+            parsed.add(Arrays.stream(chars).filter(test).map(conversion).toList());
+        }
+        return parsed;
     }
 
 }
